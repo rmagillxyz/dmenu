69ef448d1f29d58df0155bbe7e40549e8fa035dd
Robert Magill <r@magill.xyz>
2021-04-13 18:29:54 -0400

    jenny: increase fontsize

--- config.h
+++ config.h
@@ -3,11 +3,13 @@
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
+
 static const char *fonts[] = {
-	"monospace:size=13",
-	"JoyPixels:pixelsize=13:antialias=true:autohint=true"
+	"monospace:size=19",
+	"JoyPixels:pixelsize=19:antialias=true:autohint=true"
 	/* "JetBrainsMono:pixelsize=8:antialias=true:autohint=true" */
 };
+
 static const unsigned int bgalpha = 0xe0;
 static const unsigned int fgalpha = OPAQUE;
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
